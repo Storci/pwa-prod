c0a04bfec9ff91db3245e7da254c3d4e8c46fb16
simonezardi <simone.zardi@storci.com>
2022-04-04 16:14:09 +0200

    Update index.test.js

--- assets/js/index.test.js
+++ assets/js/index.test.js
@@ -1,7 +1,8 @@
-import { createCard } from "./01_Customers.js"
+const createCard = require("./01_Customers.js")
+const getCustomerList = require('./Global/Thingworx/thingworx_api_module.js')
 
 test('Effettua la creazione delle card della pagina 01_Customers', () => {
-  tw.getCustomersList()
+  getCustomerList()
   .then(customerList => {
     // Genera html della card del cliente
     expect(() => createCard(customerList)).toThrow();
