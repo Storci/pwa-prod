50eda779ae7d2148416ad6695cca5df400a978f6
simonezardi <simone.zardi@storci.com>
2022-04-06 12:23:42 +0200

    Modifica per i cambi pagina

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,11 +4,13 @@ import * as tw from "./Global/Thingworx/thingworx_api_module.js";
 // definisce l'url di base della pagina attuale (in questo caso della pagina index.html).
 // il risultato è http(s)://xxx.xxx.xxx.xxx:xxxx
 // baseURL verrà utilizzato come base per il cambio pagina.
+/*
 let baseURL = window.location.protocol + "//" + window.location.host;
 let pageURL = window.location.href
 if(window.location.protocol == 'https:'){
   baseURL += '/pwa'
 }
+*/
 
 // Recupera il nome dell'utente da firebase, controlla che sia loggato.
 // Nel caso non fosse loggato richiama la pagina di login
@@ -31,12 +33,10 @@ tw.getUser(user.email)
   // Se non è un utente storci viene reindirizzato alla pagina /Customer/Customer_Info.html.
   if(global_customer.includes("Storci")){
   	// Carica la pagina.
-		console.log(baseURL + "/01_Customers.html")
-  	//window.location.href = baseURL + "/01_Customers.html
-		window.location.href = "./01_Customers.html"
+		window.location.href = './01_Customers.html'
   }else{
   	// Carica la pagina.
-  	//window.location.href = baseURL + "/02_Dashboard.html";
+  	window.location.href = './02_Dashboard.html'
   }
 })
 .catch(error => console.error(error))
